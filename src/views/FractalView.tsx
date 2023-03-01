031de1b0fee6d9c05d56c0993461fbd3bb6edf28
Daniel Neuweiler <11644253+xSNOWM4Nx@users.noreply.github.com>
2023-03-01 22:57:16 +0100

    Fractal view updated

--- src/views/FractalView.tsx
+++ src/views/FractalView.tsx
@@ -3,7 +3,8 @@
 import React, { useRef, useState } from 'react'
 import { LogProvider } from '@daniel.neuweiler/ts-lib-module';
 import { ViewContainer, LogRenderer } from '@daniel.neuweiler/react-lib-module';
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+import { OrthographicCamera } from '@react-three/drei';
 
 import { ViewKeys } from './navigation';
 import { defaultFragmentShader } from './../shaders/fragmentShader';
@@ -16,16 +17,20 @@ function Box(props: ThreeElements['mesh']) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  useFrame((state, delta) => {
+
+    // (ref.current.rotation.x += delta)
+  })
   return (
     <mesh
       {...props}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      // scale={clicked ? 1.5 : 1}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
+      {/* <boxGeometry args={[1, 1, 1]} /> */}
+      <planeGeometry args={[2, 2]}/>
       {/* <shaderMaterial
         fragmentShader={defaultFragmentShader} /> */}
       <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
@@ -50,10 +55,19 @@ const FractalViewMemoized: React.FC<Props> = (props) => {
         })}>
 
         <Canvas>
+          <OrthographicCamera
+            makeDefault
+            left={-1}
+            right={1}
+            top={1}
+            bottom={-1}
+            near={-1}
+            far={1} />
+
           <ambientLight />
-          <pointLight position={[10, 10, 10]} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
+          {/* <pointLight position={[10, 10, 10]} /> */}
+          <Box />
+
         </Canvas>
       </div>
 
